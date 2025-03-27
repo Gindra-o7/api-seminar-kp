95b56b73b9fa2793c8d951c6f897bf5b81c3a8ad
Gindra-o7 <gilangindra734@gmail.com>
2025-03-27 10:33:01 +0700

    🔧 refactor: Replace TRPC middleware with dedicated routes for Ruangan and Jadwal

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import {createExpressMiddleware} from "@trpc/server/adapters/express"
-import {appRouter} from './trpc';
+import RuanganRoutes from './routes/ruangan.routes';
+import JadwalRoutes from './routes/jadwal.routes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/trpc", createExpressMiddleware({router: appRouter}));
+app.use('/semkp', RuanganRoutes);
+app.use('/semkp', JadwalRoutes);
 
 app.listen(3000, () => {
   console.log("Server started on http://localhost:3000");
