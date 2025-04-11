f6cbd0df6d96bc99810fbfc111d1db6987b10a09
Gindra-o7 <gilangindra734@gmail.com>
2025-04-11 09:16:56 +0700

    🔥 feat: Re-add MahasiswaRoutes to the main application for handling Mahasiswa-related requests

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import RuanganRoutes from './routes/ruangan.routes';
 import JadwalRoutes from './routes/jadwal.routes';
+import MahasiswaRoutes from './routes/mahasiswa.routes';
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use('/semkp', RuanganRoutes);
 app.use('/semkp', JadwalRoutes);
+app.use('/semkp', MahasiswaRoutes);
 
 app.listen(3000, () => {
   console.log("Server started on http://localhost:3000");
